b6dba645274a0d267ed001ef366cb6bfb030e547
shaipe <shaipe@sina.com>
2019-06-28 09:15:51 +0800

    add build

---
+++ README.md
@@ -0,0 +1,14 @@
+RUST Lang
+===
+
+## 参考文档
+
+### Web 框架
+[Nickel](http://nickel-org.github.io/)
+
+### 交叉编译
+[交叉编译linux_musl](https://www.andrew-thorburn.com/cross-compiling-a-simple-rust-web-app/)
+
+### Rust WebAssembly
+[WebAssembly + Rust 上手初探](https://www.codercto.com/a/43181.html)
+[Rust 和 WebAssembly 用例](https://developer.mozilla.org/zh-CN/docs/WebAssembly/Rust_to_wasm)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+// #[warn(unused_imports)]
+// #[macro_use] 
+
+extern crate nickel;
+
+use std::io::Write;
+use nickel::status::StatusCode::NotFound;
+use nickel::{Nickel, NickelError, Action, Continue, Halt, Request};
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    //this is how to overwrite the default error handler to handle 404 cases with a custom view
+    fn custom_404<'a>(err: &mut NickelError, _req: &mut Request) -> Action {
+        if let Some(ref mut res) = err.stream {
+            if res.status() == NotFound {
+                let _ = res.write_all(b"<h1>Call the police!</h1>");
+                return Halt(())
+            }
+        }
+
+        Continue(())
+    }
+
+
+    // issue #20178
+    let custom_handler: fn(&mut NickelError, &mut Request) -> Action = custom_404;
+
+    server.handle_error(custom_handler);
+
+    server.listen("127.0.0.1:6767").unwrap();
+
 }
